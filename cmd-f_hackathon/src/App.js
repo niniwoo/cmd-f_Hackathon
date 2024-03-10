08756ab71b6c4d676441aa74fc8690dc30fa8189
niniwoo <qhdk6914@naver.com>
2024-03-09 18:04:23 -0800

    created dummy data as well as posting detail

--- cmd-f_hackathon/src/App.js
+++ cmd-f_hackathon/src/App.js
@@ -9,6 +9,7 @@ import Main from "./components/Main";
 import Mypage from "./components/Mypage";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
+import PostingDetail from "./components/PostingDetail";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/mypage" element={<Mypage />} />
+          <Route path="/post-detail/:id" element={<PostingDetail />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
--- cmd-f_hackathon/src/components/Main.jsx
+++ cmd-f_hackathon/src/components/Main.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import urgentData from "../data/urgent";
+import regularData from "../data/regular";
 
 const Main = () => {
-  return <div>This is main page</div>;
+  const navigate = useNavigate();
+  const [showUrgent, setShowUrgent] = useState(false);
+
+  const handleUrgentClick = () => {
+    setShowUrgent(true);
+  };
+
+  const handleSeeMoreClick = () => {
+    setShowUrgent(false);
+  };
+
+  const handleClickItem = (id) => {
+    // Handle click logic here, such as navigating to a detailed view
+    console.log("Clicked item with ID:", id);
+    navigate(`/post-detail/${id}`);
+  };
+
+  return (
+    <div>
+      <h1>This is main page</h1>
+      <button onClick={handleUrgentClick}>Urgent Item</button>
+      <button onClick={handleSeeMoreClick}>See more item</button>
+      <div>
+        {showUrgent
+          ? urgentData.map((item) => (
+              <div
+                key={item.id}
+                onClick={() => handleClickItem(item.id)}
+                style={{ cursor: "pointer" }}
+              >
+                <h2>{item.name}</h2>
+                <p>Username: {item.username}</p>
+                <p>Expiry Date: {item.expiryDate}</p>
+                <p>Description: {item.description}</p>
+                <hr />
+              </div>
+            ))
+          : regularData.map((item) => (
+              <div
+                key={item.id}
+                onClick={() => handleClickItem(item.id)}
+                style={{ cursor: "pointer" }}
+              >
+                <h2>{item.name}</h2>
+                <p>Username: {item.username}</p>
+                <p>Expiry Date: {item.expiryDate}</p>
+                <p>Description: {item.description}</p>
+                <hr />
+              </div>
+            ))}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
